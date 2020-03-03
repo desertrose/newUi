6016b958fba0d8a54ace75ffada84313ac59bf9f
Naveen <f3128455@nwytg.com>
2020-03-03 23:42:29 +0530

    so far soo good

--- src/app/views/sales/sales.component.ts
+++ src/app/views/sales/sales.component.ts
@@ -6,8 +6,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent implements OnInit {
+  values = [];
+  constructor() {
 
-  constructor() { }
+
+
+    this.values = [{
+      RoutaID: '501',
+      Date: '22/12/2020',
+      Services: 'Fuel',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Gas',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Gas',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Gas',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Gas',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Cylinder',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Cylinder',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Cylinder',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Cylinder',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Cylinder',
+      Quantity: '220 Ltr'
+    }, {
+      RoutaID: '502',
+      Date: '22/12/2020',
+      Services: 'Cylinder',
+      Quantity: '220 Ltr'
+    }];
+  }
 
   ngOnInit() {
   }
